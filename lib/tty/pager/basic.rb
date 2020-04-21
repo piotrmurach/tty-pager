1e800e0e84446d93777eeacd48f5a66158d7cf78
Andrew Radev <andrey.radev@gmail.com>
2020-04-21 22:01:07 +0300

    Streaming BasicPager

--- lib/tty/pager/basic.rb
+++ lib/tty/pager/basic.rb
@@ -43,48 +43,94 @@ module TTY
       #
       # @api public
       def page(text, &callback)
-        page_num = 1
-        leftover = []
-        lines_left = @height
+        init
+        write(text, &callback)
+        wait
+      end
+
+      def init
+        @page_num = 1
+        @leftover = []
+        @lines_left = @height
+
+        @queue = Queue.new
+        @thread = Thread.new do
+          loop do
+            message = @queue.pop
+            if message.nil?
+              sleep 0.1
+              next
+            end
+
+            type    = message[0]
+            payload = message[1]
+
+            case type
+            when :quit then break
+            when :print then output.print(payload)
+            when :prompt then instance_exec(payload, &@prompt)
+            else
+              raise "Unknown message type: #{type}"
+            end
+          end
+        end
+      end
+
+      # TODO (2020-04-21) Encapsulate thread, queue, etc in a "State" struct
+      def write(text, &callback)
+        raise "Pager was not initialized" if @thread.nil? || @queue.nil? || @page_num.nil? || @leftover.nil? || @lines_left.nil?
 
         text.lines.each do |line|
           chunk = []
-          if !leftover.empty?
-            chunk = leftover
-            leftover = []
+          if !@leftover.empty?
+            chunk = @leftover
+            @leftover = []
           end
           wrapped_line = Strings.wrap(line, @width)
           wrapped_line.lines.each do |line_part|
-            if lines_left > 0
+            if @lines_left > 0
               chunk << line_part
-              lines_left -= 1
+              @lines_left -= 1
             else
-              leftover << line_part
+              @leftover << line_part
             end
           end
-          output.print(chunk.join)
+          @queue << [:print, chunk.join]
 
-          if lines_left == 0
-            break unless continue_paging?(page_num)
-            lines_left = @height
-            if leftover.size > 0
-              lines_left -= leftover.size
+          if @lines_left == 0
+            return false unless continue_paging?
+            @lines_left = @height
+            if @leftover.size > 0
+              @lines_left -= @leftover.size
             end
-            page_num += 1
-            return !callback.call(page_num) unless callback.nil?
+            @page_num += 1
+            return !callback.call(@page_num) unless callback.nil?
           end
         end
 
-        if leftover.size > 0
-          output.print(leftover.join)
+        if @leftover.size > 0
+          @queue << [:print, @leftover.join]
         end
+
+        true
+      end
+
+      def wait
+        @queue << [:quit, nil]
+        @thread.join
+
+        @queue = nil
+        @thread = nil
+        @page_num = nil
+        @leftover = nil
+        @lines_left = nil
       end
 
       private
 
       # @api private
-      def continue_paging?(page_num)
-        instance_exec(page_num, &@prompt)
+      def continue_paging?
+        @queue << [:prompt, @page_num]
         !@input.gets.chomp[/q/i]
       end
     end # BasicPager
