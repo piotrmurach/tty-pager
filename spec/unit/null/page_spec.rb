f589b668c7ffad1c0676ac1a6601096edac4c508
Piotr Murach <pmurach@gmail.com>
2020-05-22 14:51:46 +0100

    Change to ensure null pager puts call is covered in ref #19

--- spec/unit/null/page_spec.rb
+++ spec/unit/null/page_spec.rb
@@ -1,33 +1,57 @@
 # frozen_string_literal: true
 
-RSpec.describe TTY::Pager::NullPager, ".page" do
+RSpec.describe TTY::Pager::NullPager do
   let(:output)   { StringIO.new }
 
-  it "prints content to stdout when tty device" do
-    allow(output).to receive(:tty?).and_return(true)
-    pager = described_class.new(output: output)
-    text = "I try all things, I achieve what I can.\n"
+  context "page" do
+    it "prints content to stdout when tty device" do
+      allow(output).to receive(:tty?).and_return(true)
+      pager = described_class.new(output: output)
+      text = "I try all things, I achieve what I can.\n"
 
-    pager.page(text)
+      pager.page(text)
 
-    expect(output.string).to eq(text)
+      expect(output.string).to eq(text)
+    end
+
+    it "returns text when non-tty device" do
+      pager = described_class.new(output: output)
+      text = "I try all things, I achieve what I can.\n"
+
+      expect(pager.page(text)).to eq(text)
+      expect(output.string).to eq("")
+    end
+
+    it "doesn't write a newline if one wasn't given" do
+      allow(output).to receive(:tty?).and_return(true)
+      pager = described_class.new(output: output)
+      text = "I try all things, I achieve what I can."
+
+      pager.page(text)
+
+      expect(output.string).to eq(text)
+    end
   end
 
-  it "returns text when non-tty device" do
-    pager = described_class.new(output: output)
-    text = "I try all things, I achieve what I can.\n"
+  context "puts" do
+    it "writes content to terminal with a newline" do
+      allow(output).to receive(:tty?).and_return(true)
+      text = "I try all things, I achieve what I can."
+      pager = described_class.new(output: output)
 
-    expect(pager.page(text)).to eq(text)
-    expect(output.string).to eq("")
-  end
+      pager.puts(text)
 
-  it "doesn't write a newline if one wasn't given" do
-    allow(output).to receive(:tty?).and_return(true)
-    pager = described_class.new(output: output)
-    text = "I try all things, I achieve what I can."
+      expect(output.string).to eq(text + "\n")
+    end
 
-    pager.page(text)
+    it "returns content on non tty device" do
+      allow(output).to receive(:tty?).and_return(false)
+      text = "I try all things, I achieve what I can."
+      pager = described_class.new(output: output)
 
-    expect(output.string).to eq(text)
+      expect(pager.puts(text)).to eq(text)
+
+      expect(output.string).to eq("")
+    end
   end
 end
