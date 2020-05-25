5d56eceeb41c444431658a3f7bb0a4b4e2663414
Andrew Radev <andrey.radev@gmail.com>
2020-05-25 12:10:57 +0300

    Use double quotes

--- spec/integration/system_pager_spec.rb
+++ spec/integration/system_pager_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe TTY::Pager::SystemPager do
   end
 
   it "paginates some text" do
-    output_path = Pathname.new('output.txt')
-    described_class.page("Some text", command: "ruby #{fixtures_path('cat.rb')} > #{output_path}")
+    output_path = Pathname.new("output.txt")
+    described_class.page("Some text", command: "ruby #{fixtures_path("cat.rb")} > #{output_path}")
 
     expect(output_path.read).to eq "Some text"
   end
 
   it "allows pagination to happen asynchronously" do
-    output_path = Pathname.new('output.txt')
-    pager_command = "ruby #{fixtures_path('external_pager.rb')} #{output_path}"
+    output_path = Pathname.new("output.txt")
+    pager_command = "ruby #{fixtures_path("external_pager.rb")} #{output_path}"
 
     described_class.page(command: pager_command) do |pager|
       pager.puts("one")
@@ -30,8 +30,8 @@ RSpec.describe TTY::Pager::SystemPager do
   end
 
   it "stops paginating once external tool is closed" do
-    output_path = Pathname.new('output.txt')
-    pager_command = "ruby #{fixtures_path('external_pager.rb')} #{output_path}"
+    output_path = Pathname.new("output.txt")
+    pager_command = "ruby #{fixtures_path("external_pager.rb")} #{output_path}"
 
     described_class.page(command: pager_command) do |pager|
       pager.puts("one")
@@ -40,8 +40,8 @@ RSpec.describe TTY::Pager::SystemPager do
       # wait for script to finish writing
       sleep 0.5
 
-      pid = pager.instance_variable_get('@pager_io').instance_variable_get('@pid')
-      Process.kill('KILL', pid)
+      pid = pager.instance_variable_get("@pager_io").instance_variable_get("@pid")
+      Process.kill("KILL", pid)
       Process.waitpid(pid)
 
       # Script finishes after two lines, this `puts` call should raise `PagerClosed`
